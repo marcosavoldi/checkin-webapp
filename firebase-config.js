92d0eefad6e724315c76b4f2938ac9e104b15a9a
Marco <marco.savo.92@gmail.com>
2025-11-01 11:56:40 +0100

    Add firebase config

---
+++ firebase-config.js
@@ -0,0 +1,10 @@
+// Firebase Configuration
+export const firebaseConfig = {
+  apiKey: "AIzaSyAeTLg4CRGvUFk6d47048Xn6CxMYdL6_hA",
+  authDomain: "passport-manager-85f02.firebaseapp.com",
+  projectId: "passport-manager-85f02",
+  storageBucket: "passport-manager-85f02.firebasestorage.app",
+  messagingSenderId: "836278649997",
+  appId: "1:836278649997:web:48675d2993a5de11424ec4",
+  measurementId: "G-J57Q8D9Y18"
+};
